eaf609d0159646c835e696c4aa4e9d7623ca1432
Mona <mgit94684@outlook.com>
2024-10-27 21:18:51 +0100

    slettet en duplikat av Mappen

--- SET-Gruppe-10/SmartHouse/src/main/java/no/SetGruppe10/models/SmartDoorController.java
+++ SET-Gruppe-10/SmartHouse/src/main/java/no/SetGruppe10/models/SmartDoorController.java
@@ -1,4 +1,8 @@
 package no.SetGruppe10.models;
 
-public class SmartDoorController {
+public class SmartDoorController extends DeviceFoundation {
+
+    public SmartDoorController(String deviceName, Boolean isDeviceActive) {
+        super(deviceName, isDeviceActive);
+    }
 }
